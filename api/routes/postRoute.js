e955f2ddaf72e350c2ec84021ce526b231d30ae7
“oussama <oussama.oulaydi@gmail.com>
2024-08-27 12:34:01 +0100

    Add auth, post and user route

---
+++ api/routes/postRoute.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const router = express.Router();
+
+// router.get('/test', (req, res) => {
+//     res.json({ message: 'Hello from the server side!' });
+// });
+
+// router.post('/test', (req, res) => {
+//     res.json({ message: 'Hello from the server side!' });
+// });
+
+// router.put('/test', (req, res) => {
+//     res.json({ message: 'Hello from the server side!' });
+// });
+
+// router.delete('/test', (req, res) => {
+//     res.json({ message: 'Hello from the server side!' });
+// });
+
+export default router;
