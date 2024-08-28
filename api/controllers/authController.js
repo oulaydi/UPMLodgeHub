5fc40de201ae71823bb8a09506cfe247a2d045eb
“oussama <oussama.oulaydi@gmail.com>
2024-08-28 11:20:10 +0100

    Get phone number during registertaion as well, using it for twilio verification

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 
 
 export const register = async (req, res) => {
-    const { username, email, password } = req.body;
+    const { username, email, phone, password } = req.body;
 
     try {
         // Hash pwd
@@ -14,6 +14,7 @@ export const register = async (req, res) => {
             data: {
                 username,
                 email,
+                phone,
                 password: hashedPassword,
             },
         });
