5264e0dc019b206afd9123491e89c3ee7c8e7e9c
“oussama <oussama.oulaydi@gmail.com>
2024-08-28 10:43:19 +0100

    Add some packages as well 'cookie parser'

--- api/app.js
+++ api/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import postRoute from './routes/postRoute.js';
 import authRoute from './routes/authRoute.js';
 
-const app = express();;
-
+const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use('/api/auth/', authRoute);
 
 app.listen(2024, () => {
