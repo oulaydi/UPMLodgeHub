052e24b494314de560fcf8922bfc9bc5e888a4ad
“oussama <oussama.oulaydi@gmail.com>
2024-09-08 19:06:56 +0100

    Everything works good as well

--- api/app.js
+++ api/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import postRoute from './routes/postRoute.js';
 import authRoute from './routes/authRoute.js';
+import userRoute from './routes/userRoute.js';
+import postRoute from './routes/postRoute.js';
 import testRoute from './routes/testRoute.js';
 
 const app = express();
@@ -12,8 +13,9 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use('/api/posts/', postRoute);
 app.use('/api/auth/', authRoute);
+app.use('/api/users/', userRoute);
+app.use('/api/posts/', postRoute);
 app.use('/api/test/', testRoute);
 
 app.listen(2024, () => {
